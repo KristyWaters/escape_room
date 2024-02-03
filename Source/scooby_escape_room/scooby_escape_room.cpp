038b500631eea42bd11f2277eae5b4a9433c07b3
KristyWaters <Waters.KristyL@gmail.com>
2024-02-03 12:28:10 -0600

    Deleted original project

--- Source/scooby_escape_room/scooby_escape_room.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "scooby_escape_room.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, scooby_escape_room, "scooby_escape_room" );
- 
